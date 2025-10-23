8b7cf6aa335fc60f87356b06b4196bd9bff73108
Adrian Yudhaa <yudhaa.belajar@gmail.com>
2025-10-23 15:35:29 +0700

    Fix: Remove static export config for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // <<< TAMBAHKAN ATAU UBAH BARIS INI
+  // output: 'export', // <<< TAMBAHKAN ATAU UBAH BARIS INI
   eslint: {
     ignoreDuringBuilds: true,
   },
